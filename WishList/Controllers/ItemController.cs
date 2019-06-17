db7a596844b4d808335c61ab9389328b666403a3
Chuck R Fraley <112918@CARMAX.COM>
2019-06-17 16:09:47 -0400

    c

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -21,5 +21,27 @@ namespace WishList.Controllers
             var model = _context.Items.ToList();
             return View("Index", model);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Models.Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete (int id)
+        {
+            var item = _context.Items.FirstOrDefault(e => e.Id == id);
+            _context.Items.Remove(item);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
